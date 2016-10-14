2a7e10f1b01b1e7e683349b29bbc66db813ff000
Andrew Westrope <andrew@warwickshire.it>
2016-10-14 20:56:31 +0100

    Add allTenants

--- src/Warksit/Tenant/TenantTrait.php
+++ src/Warksit/Tenant/TenantTrait.php
@@ -8,4 +8,16 @@ trait TenantTrait {
     {
         static::addGlobalScope(\App::make('Warksit\Tenant\TenantScope'));
     }
+
+    /**
+     * Returns a new builder without the tenant scope applied.
+     *
+     *     $allUsers = User::allTenants()->get();
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public static function allTenants()
+    {
+        return with(new static)->newQueryWithoutScope(\App::make(TenantScope::class));
+    }
 }
